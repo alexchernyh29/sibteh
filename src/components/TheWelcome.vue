2edd9c010de31881206de694f9e0728a1bcb787c
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-12-12 22:14:27 +0300

    delete ecosystem

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,21 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
 
   
 
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
+ 
 
   <WelcomeItem>
     <template #icon>
