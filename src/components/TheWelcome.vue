67b1e0393e897495ebaae3ed88a414af19fb1ef2
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-12-12 22:15:19 +0300

    delete support vue

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -16,14 +16,5 @@ import SupportIcon from './icons/IconSupport.vue'
 
   
 
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
-  </WelcomeItem>
+  
 </template>
