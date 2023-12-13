690aaaaddf1c00fb713d7db1356092526c45675e
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-12-13 22:34:34 +0300

    add div eding form page

--- src/views/FormEdingView.vue
+++ src/views/FormEdingView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-
+    <div class="edit">
+      <h1>This is an form page</h1>
     </div>
 </template>
 
@@ -9,5 +9,11 @@
 </script>
 
 <style lang="scss" scoped>
-
+@media (min-width: 1024px) {
+    .edit {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
 </style>
