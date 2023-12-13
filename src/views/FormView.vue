534500aab650311dd938f791cdfa2e08cc6c8585
Alexander Chernyh <alexchernyh2000@mail.ru>
2023-12-13 22:08:08 +0300

    add templete FormView

---
+++ src/views/FormView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="form">
+      <h1>This is an form page</h1>
+    </div>
+  </template>
+  
+<script>
+
+</script>
+
+<style>
+@media (min-width: 1024px) {
+    .form {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
